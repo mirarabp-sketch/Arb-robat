94d659f7c605e3ea5f4286b7a34638e385969a99
mirarabp-sketch <mirarab.p@gmail.com>
2026-07-11 17:59:10 +0330

    Create tokenConfig.js

---
+++ Backend/tokenConfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  DEGEN: {
+    symbol: "DEGEN",
+    decimals: 18,
+    network: "Base"
+  },
+
+  USDC: {
+    symbol: "USDC",
+    decimals: 6,
+    network: "Base"
+  }
+
+};
