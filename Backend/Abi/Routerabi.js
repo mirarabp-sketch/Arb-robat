83e19e5fe8ce5d749eefec95512c867858edbec6
mirarabp-sketch <mirarab.p@gmail.com>
2026-07-11 16:36:06 +0330

    Create Routerabi.js

---
+++ Backend/Abi/Routerabi.js
@@ -0,0 +1,4 @@
+module.exports = [
+  "function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts)",
+  "function swapExactTokensForTokens(uint amountIn,uint amountOutMin,address[] calldata path,address to,uint deadline) external returns (uint[] memory amounts)"
+];
