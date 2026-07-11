17abb8652eaa8feef84c62971867be7a7c735a11
mirarabp-sketch <mirarab.p@gmail.com>
2026-07-11 16:50:32 +0330

    Update and rename Contracts.js to Backend/Contracts.js

--- Backend/Contracts.js
+++ Backend/Contracts.js
@@ -2,17 +2,11 @@ module.exports = {
   BASE_CHAIN_ID: 8453,
 
   DEGEN: "0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed",
-
-  // USDC روی Base
   USDC: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
 
   ROUTERS: {
-    // Uniswap Universal Router
     Uniswap: "0x198EF79F1F515F02dFE9e3115eD9fC07183f02fC",
-
-    // در مرحله بعد آدرس‌های رسمی Aerodrome و SushiSwap
-    // را هم اضافه می‌کنیم.
-    Aerodrome: "",
+    Aerodrome: "0xcF77a3Ba9A5CA399B7c97c74d54e5b1Beb874E43",
     SushiSwap: ""
   }
 };
