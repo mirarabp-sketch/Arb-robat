f0bbf25df1f9ae9ef0991c69d7e7c755f8c3107b
mirarabp-sketch <mirarab.p@gmail.com>
2026-07-11 18:27:13 +0330

    Create dexConfig.js

---
+++ Backend/dexConfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  sushiSwap: {
+    name: "SushiSwap",
+    router: process.env.SUSHISWAP_ROUTER
+  },
+
+
+  aerodrome: {
+    name: "Aerodrome",
+    router: process.env.AERODROME_ROUTER
+  }
+
+};
