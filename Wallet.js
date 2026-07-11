eb93ecf477d30fd05123d2b04c88d933d742b78c
mirarabp-sketch <mirarab.p@gmail.com>
2026-07-11 16:29:21 +0330

    Create Wallet.js

---
+++ Wallet.js
@@ -0,0 +1,14 @@
+const { ethers } = require("ethers");
+require("dotenv").config();
+
+const provider = new ethers.JsonRpcProvider(process.env.BASE_RPC);
+
+const wallet = new ethers.Wallet(
+  process.env.PRIVATE_KEY,
+  provider
+);
+
+module.exports = {
+  provider,
+  wallet
+};
