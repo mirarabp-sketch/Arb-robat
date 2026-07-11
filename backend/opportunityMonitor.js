07e8a6554c8f9526fedd6853d7948d3ab03fe473
mirarabp-sketch <mirarab.p@gmail.com>
2026-07-11 17:08:33 +0330

    Create opportunityMonitor.js

---
+++ backend/opportunityMonitor.js
@@ -0,0 +1,36 @@
+const { findOpportunity } = require("./arbitrageEngine");
+const { saveTrade } = require("./tradeHistory");
+
+
+function checkOpportunity(prices) {
+
+  const opportunity = findOpportunity(prices);
+
+
+  if (!opportunity) {
+    console.log("No opportunity");
+    return null;
+  }
+
+
+  console.log("Opportunity Found:");
+  console.log(opportunity);
+
+
+  if (Number(opportunity.profitPercent) > 0.5) {
+
+    saveTrade({
+      type: "OPPORTUNITY",
+      ...opportunity
+    });
+
+  }
+
+
+  return opportunity;
+}
+
+
+module.exports = {
+  checkOpportunity
+};
