741eabde27e4dfcf11c9f2de6ff2d748a077ad6d
mirarabp-sketch <mirarab.p@gmail.com>
2026-07-11 18:27:45 +0330

    Create tokenAddresses.js

---
+++ Backend/tokenAddresses.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  DEGEN: {
+    address: "0x0000000000000000000000000000000000000000",
+    symbol: "DEGEN"
+  },
+
+  USDC: {
+    address: "0x0000000000000000000000000000000000000000",
+    symbol: "USDC"
+  }
+
+};
