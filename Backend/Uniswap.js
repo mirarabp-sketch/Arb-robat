f72eeb6dda04c0b2a64bd21e13b115921f46661b
mirarabp-sketch <mirarab.p@gmail.com>
2026-07-11 16:43:42 +0330

    Update Uniswap.js

--- Backend/Uniswap.js
+++ Backend/Uniswap.js
@@ -1,24 +1,35 @@
 const { ethers } = require("ethers");
 const { provider } = require("./wallet");
-const contracts = require("./contracts");
-const routerAbi = require("./abi/routerAbi");
 
-const router = new ethers.Contract(
-    contracts.ROUTERS.Uniswap,
-    routerAbi,
-    provider
+const QUOTER_V2 = "0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a";
+
+const quoterAbi = [
+  "function quoteExactInputSingle((address tokenIn,address tokenOut,uint256 amountIn,uint24 fee,uint160 sqrtPriceLimitX96)) external returns (uint256 amountOut,uint160 sqrtPriceX96After,uint32 initializedTicksCrossed,uint256 gasEstimate)"
+];
+
+const quoter = new ethers.Contract(
+  QUOTER_V2,
+  quoterAbi,
+  provider
 );
 
-async function getPrice(amountIn, path) {
-    try {
-        const amounts = await router.getAmountsOut(amountIn, path);
-        return amounts;
-    } catch (err) {
-        console.error(err);
-        return null;
-    }
+async function getQuote(tokenIn, tokenOut, amountIn, fee = 3000) {
+  try {
+    const result = await quoter.quoteExactInputSingle({
+      tokenIn,
+      tokenOut,
+      amountIn,
+      fee,
+      sqrtPriceLimitX96: 0
+    });
+
+    return result.amountOut;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
 
 module.exports = {
-    getPrice
+  getQuote
 };
