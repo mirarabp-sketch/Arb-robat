0df96c186f19008ac3a207687a2663b50449084c
mirarabp-sketch <mirarab.p@gmail.com>
2026-07-11 16:32:45 +0330

    Create Contracts.js

---
+++ Contracts.js
@@ -0,0 +1,13 @@
+module.exports = {
+  BASE_CHAIN_ID: 8453,
+
+  DEGEN: "0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed",
+
+  USDC: "",
+
+  ROUTERS: {
+    Aerodrome: "",
+    SushiSwap: "",
+    Uniswap: ""
+  }
+};
