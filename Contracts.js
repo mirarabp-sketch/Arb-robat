e742436ea6be98b3489b6878d34e427a245f2b43
mirarabp-sketch <mirarab.p@gmail.com>
2026-07-11 16:33:30 +0330

    Update Contracts.js

--- Contracts.js
+++ Contracts.js
@@ -3,11 +3,16 @@ module.exports = {
 
   DEGEN: "0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed",
 
-  USDC: "",
+  // USDC روی Base
+  USDC: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
 
   ROUTERS: {
+    // Uniswap Universal Router
+    Uniswap: "0x198EF79F1F515F02dFE9e3115eD9fC07183f02fC",
+
+    // در مرحله بعد آدرس‌های رسمی Aerodrome و SushiSwap
+    // را هم اضافه می‌کنیم.
     Aerodrome: "",
-    SushiSwap: "",
-    Uniswap: ""
+    SushiSwap: ""
   }
 };
